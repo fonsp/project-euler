1cd25900a8ee6c7005879c1f11ea922043f9069e
Fons van der Plas <fonsvdplas@gmail.com>
2016-05-17 01:03:16 +0200

    Problems 45 and 46 solved

---
+++ Problem 45/Problem 45/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EulerDotNet;
+
+
+namespace Problem_45
+{
+	class Program
+	{
+		[STAThread]
+		static void Main(string[] args)
+		{
+			long t=0, p=0, h=0;
+			long tValue=1, pValue=1, hValue=1;
+			while(true)
+			{
+				long max = Math.Max(tValue, Math.Max(pValue, hValue));
+				if(tValue == pValue && pValue == hValue)
+				{
+					if(max > 40755)
+					{
+						EMisc.End(max);
+					}
+					Console.WriteLine(max);
+					max++;
+				}
+				while(tValue < max)
+				{
+					t++;
+					tValue = EMath.TriangularNumber(t);
+				}
+				while(pValue < max)
+				{
+					p++;
+					pValue = EMath.PentagonalNumber(p);
+				}
+				while(hValue < max)
+				{
+					h++;
+					hValue = EMath.HexagonalNumber(h);
+				}
+			}
+		}
+	}
+}
---
+++ Problem 46/Problem 46/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EulerDotNet;
+
+namespace Problem_46
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			long i = 9;
+			while(true)
+			{
+				i += 2;
+				if(!EMath.IsPrime(i))
+				{
+					if(!Check(i))
+					{
+						EMisc.End(i);
+					}
+				}
+			}
+		}
+
+		static bool Check(long i)
+		{
+			long doubleSquare = 0;
+			long x = 1;
+			while(doubleSquare < i)
+			{
+				doubleSquare = 2 * x * x;
+				int index = 0;
+				long sum = 0;
+				while(sum < i)
+				{
+					sum = EMath.GetPrime(index) + doubleSquare;
+                    index++;
+				}
+				if(sum == i)
+				{
+					return true;
+				}
+				x++;
+			}
+			return false;
+		}
+	}
+}
