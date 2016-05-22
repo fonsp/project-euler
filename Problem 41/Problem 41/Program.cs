a5af8cb7c6b28f514eaf34c7c64589336ab01f82
Fons van der Plas <fonsvdplas@gmail.com>
2016-05-22 02:00:06 +0200

    Problems 41 and 47 solved

---
+++ Problem 41/Problem 41/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EulerDotNet;
+
+namespace Problem_41
+{
+	class Program
+	{
+		[STAThread]
+		static void Main(string[] args)
+		{
+			for(int n = 9; n > 0; n--)
+			{
+				int[] digits = new int[n];
+				byte[] dirs = new byte[n];
+				for(int i = 0; i < n; i++)
+				{
+					digits[i] = i + 1;
+				}
+				long largestPrime = -1;
+				while(EMisc.IteratePermutation(digits, dirs, -1, n, -1))
+				{
+					long x = EMath.FromDigits(digits);
+					if(x > largestPrime && EMath.IsPrime(x))
+					{
+						largestPrime = x;
+					}
+				}
+				if(largestPrime != -1)
+				{
+					EMisc.End(largestPrime);
+				}
+			}
+		}
+	}
+}
---
+++ Problem 47/Problem 47/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EulerDotNet;
+
+namespace Problem_47
+{
+	class Program
+	{
+		[STAThread]
+		static void Main(string[] args)
+		{
+			int count = 0;
+			for(long i = 1;; i++)
+			{
+				if(EMath.GetPrimeFactors(i).Distinct().Count() == 4)
+				{
+					count++;
+					if(count == 4)
+					{
+						EMisc.End(i - 3);
+					}
+				}
+				else
+				{
+					count = 0;
+				}
+			}
+		}
+	}
+}
