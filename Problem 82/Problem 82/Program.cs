2d8cb98e6c9005409e9b0aa20dc7a1214f15dc25
Fons van der Plas <fonsvdplas@gmail.com>
2016-10-22 16:03:11 +0200

    Problem 89 solved

---
+++ Problem 82/Problem 82/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EulerDotNet;
+using System.IO;
+
+namespace Problem_82
+{
+	class Program
+	{
+		[STAThread]
+		static void Main(string[] args)
+		{
+			loadMatrix("matrix.txt");
+			for(int i = 0; i < N; i++)
+			{
+				scores[i, 0] = matrix[i, 0];
+			}
+			for(int j = 1; j < N; j++)
+			{
+				for(int i = 0; i < N; i++)
+				{
+					scores[i, j] = scores[i, j - 1] + matrix[i, j];
+				}
+				for(int i = 1; i < N; i++)
+				{
+					scores[i, j] = Math.Min(scores[i, j], scores[i - 1, j] + matrix[i, j]);
+				}
+				for(int i = N - 2; i >= 0; i--)
+				{
+					scores[i, j] = Math.Min(scores[i, j], scores[i + 1, j] + matrix[i, j]);
+				}
+			}
+			int min = int.MaxValue;
+			for(int i = 0; i < N; i++)
+			{
+				min = Math.Min(scores[i, N - 1], min);
+			}
+			EMisc.End(min);
+		}
+
+		static int getScore(int i, int j)
+		{
+			if(i < 0 || j < 0 || i >= N || j >= N)
+			{
+				return int.MaxValue / 2;
+			}
+			return scores[i, j];
+		}
+
+		static int[,] matrix;
+		static int[,] scores;
+		static int N;
+
+		static void loadMatrix(string path)
+		{
+			string[] lines = File.ReadAllLines(path);
+			N = lines.Length;
+			matrix = new int[N, N];
+			scores = new int[N, N];
+
+			for(int i = 0; i < N; i++)
+			{
+				string[] line = lines[i].Split(new char[] { ',' });
+				for(int j = 0; j < N; j++)
+				{
+					matrix[i, j] = int.Parse(line[j]);
+				}
+			}
+		}
+
+	}
+}
---
+++ Problem 89/Problem 89/Program.cs
@@ -0,0 +1,91 @@
+﻿using EulerDotNet;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_89
+{
+	class Program
+	{
+		[STAThread]
+		static void Main(string[] args)
+		{
+			int savings = 0;
+			foreach(string s in File.ReadAllLines("roman.txt"))
+			{
+				savings += s.Length;
+				savings -= ToRoman(ToInt(s)).Length;
+			}
+			EMisc.End(savings);
+		}
+
+		static int ToInt(string r)
+		{
+			int n = 0;
+			int i = r.Length - 1;
+			while(i > 0)
+			{
+				int a = GetChar(r[i]);
+				int b = GetChar(r[i - 1]);
+				if(b < a)
+				{
+					n += a - b;
+					i -= 2;
+				}
+				else
+				{
+					n += a;
+					i--;
+				}
+			}
+			if(i == 0)
+			{
+				n += GetChar(r[0]);
+			}
+			return n;
+		}
+
+		static int GetChar(char c)
+		{
+			switch(c)
+			{
+				case 'I':
+					return 1;
+				case 'V':
+					return 5;
+				case 'X':
+					return 10;
+				case 'L':
+					return 50;
+				case 'C':
+					return 100;
+				case 'D':
+					return 500;
+				case 'M':
+					return 1000;
+			}
+			return -1;
+		}
+
+		static string ToRoman(int n)
+		{
+			string output = "";
+			string[] numerals = new string[] {"M","CM","D","CD","C","XC", "L", "XL", "X", "IX"};
+			foreach(string numeral in numerals)
+			{
+				int bound = ToInt(numeral);
+				while(n >= bound)
+				{
+					n -= bound;
+					output += numeral;
+				}
+			}
+			return output + finalRomans[n];
+		}
+
+		static string[] finalRomans = new string[] { "", "I", "II", "III", "IV", "V", "VI", "VII", "VIII" };
+	}
+}
